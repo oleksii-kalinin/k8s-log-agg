a605a1f0fc8967e20da6b1eb18b555d6dd94e2cd
Oleksii Kalinin <kaliha91@gmail.com>
2026-01-18 12:46:04 +0100

    Add Kubernetes pod log streaming with signal handling (#3)
    Init stream logs

--- main.go
+++ main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"context"
+	"errors"
 	"flag"
+	"io"
 	"log"
 	"os"
+	"os/signal"
 	"path/filepath"
+	"syscall"
 
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	v2 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
@@ -16,10 +21,26 @@ import (
 func main() {
 	var labels string
 	var namespace string
+	var follow bool
+	var pod string
 
-	flag.StringVar(&labels, "labels", "", "set labels for the selector.")
-	flag.StringVar(&namespace, "namespace", "default", "namespace")
+	flag.StringVar(&labels, "labels", "", "set labels for the selector")
+	flag.StringVar(&namespace, "namespace", "default", "namespace name")
+	flag.BoolVar(&follow, "follow", false, "follow logs from the pod or exit")
+	flag.StringVar(&pod, "pod", "", "pod name for logs streaming")
 	flag.Parse()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	ctx := context.Background()
+	cancelCtx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	go func() {
+		<-sigChan
+		cancel()
+	}()
 
 	kubeconfigPath := os.Getenv("KUBECONFIG")
 	home := homedir.HomeDir()
@@ -37,16 +58,37 @@ func main() {
 		panic(err.Error())
 	}
 
-	pods, err := clientset.CoreV1().Pods(namespace).List(context.Background(), v1.ListOptions{
-		LabelSelector: labels,
+	//pods, err := clientset.CoreV1().Pods(namespace).List(cancelCtx, v1.ListOptions{
+	//	LabelSelector: labels,
+	//})
+	//if err != nil {
+	//	panic(err.Error())
+	//}
+	//if len(pods.Items) == 0 {
+	//	log.Println("no pods found")
+	//	return
+	//}
+	logsReq := clientset.CoreV1().Pods(namespace).GetLogs(pod, &v2.PodLogOptions{
+		Follow: follow,
 	})
+	logsResp, err := logsReq.Stream(cancelCtx)
 	if err != nil {
+		if apierrors.IsNotFound(err) {
+			log.Fatalln("pod not found")
+		}
+		if apierrors.IsBadRequest(err) {
+			log.Fatalf("Invalid request: %v", err)
+		}
+		log.Fatalln(err.Error())
+	}
+	defer logsResp.Close()
+
+	_, err = io.Copy(os.Stdout, logsResp)
+	if err != nil {
+		if errors.Is(err, context.Canceled) {
+			log.Print("exiting")
+			return
+		}
 		panic(err.Error())
-	}
-	if len(pods.Items) == 0 {
-		log.Println("no pods found")
-	}
-	for _, v := range pods.Items {
-		log.Printf("pod: %s, namespace: %s, labels: %s", v.Name, v.Namespace, v.Labels)
 	}
 }
