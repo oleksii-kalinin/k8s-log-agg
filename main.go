6af65768cf112dca5e83a0acfcbbf7982113d6fc
Oleksii Kalinin <kaliha91@gmail.com>
2026-01-19 18:31:39 +0100

    Support concurrent log streaming for multiple pods (#4)
    * Streaming

--- main.go
+++ main.go
@@ -1,22 +1,29 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"errors"
 	"flag"
-	"io"
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
 	"path/filepath"
+	"sync"
 	"syscall"
 
 	v2 "k8s.io/api/core/v1"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 )
+
+type LogLine struct {
+	PodName string
+	Log     string
+}
 
 func main() {
 	var labels string
@@ -48,9 +55,14 @@ func main() {
 		kubeconfigPath = filepath.Join(home, ".kube", "config")
 	}
 
+	if pod == "" && labels == "" {
+		log.Fatalln("No labels provided (required when --pod is omitted)")
+		return
+	}
+
 	config, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
 	if err != nil {
-		panic(err.Error())
+		log.Fatalf("Error loading kubeconfig: %v", err)
 	}
 
 	clientset, err := kubernetes.NewForConfig(config)
@@ -58,37 +70,97 @@ func main() {
 		panic(err.Error())
 	}
 
-	//pods, err := clientset.CoreV1().Pods(namespace).List(cancelCtx, v1.ListOptions{
-	//	LabelSelector: labels,
-	//})
-	//if err != nil {
-	//	panic(err.Error())
-	//}
-	//if len(pods.Items) == 0 {
-	//	log.Println("no pods found")
-	//	return
-	//}
+	logChan := make(chan LogLine, 100)
+	var wg sync.WaitGroup
+	var logWg sync.WaitGroup
+
+	logWg.Add(1)
+	go func() {
+		defer logWg.Done()
+		for logLine := range logChan {
+			fmt.Printf("[%s] %s\n", logLine.PodName, logLine.Log)
+		}
+	}()
+
+	// Get pods list for given labels
+	if pod == "" {
+
+		podList, err := clientset.CoreV1().Pods(namespace).List(cancelCtx, v1.ListOptions{
+			LabelSelector: labels,
+		})
+		if err != nil {
+			log.Fatalf("Error listing pods: %v", err)
+		}
+
+		if len(podList.Items) == 0 {
+			log.Fatalln("No pod found")
+		}
+		log.Printf("Found %d pods", len(podList.Items))
+		if follow {
+			log.Println("Streaming logs, press Ctrl+C to exit")
+		}
+
+		for _, p := range podList.Items {
+			wg.Add(1)
+			go func(pod v2.Pod) {
+				defer wg.Done()
+				log.Printf("Starting stream for pod %s", pod.Name)
+				if err := streamPodLogs(cancelCtx, clientset, pod.Namespace, pod.Name, follow, logChan); err != nil {
+					if !errors.Is(err, context.Canceled) {
+						log.Printf("Error streaming pod %s: %v", pod.Name, err)
+					}
+				}
+			}(p)
+		}
+	} else {
+		if follow {
+			log.Println("Streaming logs, press Ctrl+C to exit")
+			log.Printf("Starting stream for pod %s", pod)
+		}
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			err = streamPodLogs(cancelCtx, clientset, namespace, pod, follow, logChan)
+			if err != nil {
+				if errors.Is(err, context.Canceled) {
+					log.Print("exiting")
+					return
+				}
+				log.Printf("Error reading logs: %v", err)
+			}
+		}()
+	}
+	if follow {
+		<-cancelCtx.Done()
+	}
+
+	wg.Wait()
+	close(logChan)
+
+	logWg.Wait()
+
+	log.Println("Shutting down...")
+}
+
+func streamPodLogs(ctx context.Context, clientset *kubernetes.Clientset, namespace, pod string, follow bool, logChan chan<- LogLine) error {
 	logsReq := clientset.CoreV1().Pods(namespace).GetLogs(pod, &v2.PodLogOptions{
 		Follow: follow,
 	})
-	logsResp, err := logsReq.Stream(cancelCtx)
+	logsResp, err := logsReq.Stream(ctx)
 	if err != nil {
-		if apierrors.IsNotFound(err) {
-			log.Fatalln("pod not found")
-		}
-		if apierrors.IsBadRequest(err) {
-			log.Fatalf("Invalid request: %v", err)
-		}
-		log.Fatalln(err.Error())
+		return fmt.Errorf("failed to stream pod '%s': %w", pod, err)
 	}
 	defer logsResp.Close()
 
-	_, err = io.Copy(os.Stdout, logsResp)
-	if err != nil {
-		if errors.Is(err, context.Canceled) {
-			log.Print("exiting")
-			return
+	buf := make([]byte, 0, 64*1024)
+	scanner := bufio.NewScanner(logsResp)
+	scanner.Buffer(buf, 1024*1024)
+
+	for scanner.Scan() {
+		logChan <- LogLine{
+			Log:     scanner.Text(),
+			PodName: pod,
 		}
-		panic(err.Error())
 	}
+	return scanner.Err()
 }
